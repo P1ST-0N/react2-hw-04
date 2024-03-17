2262f28908cae76ee19e308b91a438867923aa3a
P1ST-0N <luckylionia@gmail.com>
2024-03-17 19:03:55 +0200

    Update rest-api.js

--- src/api/rest-api.js
+++ src/api/rest-api.js
@@ -6,7 +6,7 @@ const searchImages = async (query, page) => {
     const response = await axios.get(
       `https://api.unsplash.com/search/photos?client_id=${API_KEY}&page=${page}&per_page=12&query=${query}`
     );
-    console.log(response.data.results);
+    // console.log(response.data.results);
     return response.data.results;
   } catch (error) {
     console.error("Ошибка при запросе данных:", error);
